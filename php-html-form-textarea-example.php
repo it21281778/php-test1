ad44eac28865f0e0749d6163270199a5ed6325d0
P.K.D.L.W.Alwis <100864688+it21281778@users.noreply.github.com>
2022-06-08 11:46:45 +0530

    Add files via upload

---
+++ php-html-form-textarea-example.php
@@ -0,0 +1,29 @@
+<?php
+
+//require "config.php";
+
+$target_dir = "uploads/";
+
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+
+$uploadOk = 1;
+
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+    && $imageFileType != "gif" ) {
+      echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+      $uploadOk = 0;
+    }
+  else 
+  {
+    echo "File is not an image.";
+    $uploadOk = 0;
+ }
+}
+
+?>
